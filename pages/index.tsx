57de964da98ca5bca09285c9ddea60cb4f9a4977
Subham Ray <self.programmed@gmail.com>
2022-05-07 11:25:08 +0530

    tailwind base styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-2x text-blue-500">Tailwind Works!</div>;
+  return <div className="text-2xl ">Tailwind Works!</div>;
 };
 
 export default Home;
