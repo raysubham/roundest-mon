efb4f04c036800283a2ba53979cc219126246a90
Subham Ray <self.programmed@gmail.com>
2022-05-07 13:04:33 +0530

    basic structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-2xl ">Tailwind Works!</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center">
+      <div className="text2xl text-center">Which Pokemon is the roundest?</div>
+      <div className="max-w-2xl mt-4 mx-auto p-8 border rounded flex justify-between items-center gap-2">
+        <div className="p-2 px-4 border">1</div>
+        <p>vs</p>
+        <div className="p-2 px-4 border">2</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
